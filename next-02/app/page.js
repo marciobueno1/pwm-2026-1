1e31822c2ef63ed58443ab7094f2e126c4c4ba66
Marcio Bueno <marciobueno@gmail.com>
2026-03-17 08:53:19 -0300

    useState - estilo condicional - renderização de listas

--- next-02/app/page.js
+++ next-02/app/page.js
@@ -1,5 +1,19 @@
 //import styles from "./page.module.css";
+import MeuBotao from "@/components/MyButton";
+import { MyButton2 as MeuBotao2 } from "@/components/MyButton2";
+import MyList from "@/components/MyList";
 
 export default function Home() {
-  return <div>Olá</div>;
+  return (
+    <>
+      <h1 className="titulo">Welcome to my app</h1>
+      <MeuBotao title="Eu sou o botão 1" />
+      <MeuBotao title="Eu sou outro botão 1" />
+      <hr />
+      <MeuBotao2 />
+      <MeuBotao2 />
+      <hr />
+      <MyList />
+    </>
+  );
 }
