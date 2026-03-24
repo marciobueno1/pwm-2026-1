c4f01fd9a36ba86cdb4eaed59d2cc0c4a7097bb8
Marcio Bueno <marciobueno@gmail.com>
2026-03-24 08:43:25 -0300

    configurando o tanstack query (GET e POST)

--- next-02/api/index.js
+++ next-02/api/index.js
@@ -5,9 +5,22 @@ const headers = {
   "X-Parse-Application-Id": "ortn41B33LH89PU5JexteP1etYzjfH6AdtSi6CHb",
   "X-Parse-REST-API-Key": "5wPkNP3a0Aq3Kbn2ddeFTQOBVDaMT0giiSGYt5Sx",
 };
+const headersJson = {
+  ...headers,
+  "Content-Type": "application/json",
+};
 
 export const getTasks = async () => {
-  return await axios.get(urlBase, {
+  const { data } = await axios.get(urlBase, {
     headers,
   });
+  return data;
+};
+
+export const addTask = async (newTask) => {
+  const { data } = await axios.post(urlBase, newTask, {
+    headers: headersJson,
+  });
+  console.log("retorno da nova task vinda do back4app: ", data);
+  return data;
 };
--- next-02/app/myTodoList/page.js
+++ next-02/app/myTodoList/page.js
@@ -2,20 +2,38 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { getTasks } from "@/api";
+import { addTask, getTasks } from "@/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export default function MyTodoList() {
-  const [tasks, setTasks] = useState([]);
+  const queryClient = useQueryClient();
+  const { data, isFetching, error } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: getTasks,
+  });
+  const mutation = useMutation({
+    mutationFn: addTask,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
   const [description, setDescription] = useState("");
 
   function handleSubmit(evt) {
     evt.preventDefault();
-    setTasks([...tasks, description]);
+    if (!description) {
+      alert("Descrição é um campo obrigatório!");
+      return;
+    }
+    mutation.mutate({ description });
     setDescription("");
   }
   return (
     <>
       <h1>Lista de Tarefas</h1>
+      <hr />
+      {error && <h3>Erro: {error}</h3>}
+      {isFetching && <h3>Carregando dados do servidor...</h3>}
       <hr />
       <form onSubmit={handleSubmit}>
         <input
@@ -23,13 +41,15 @@ export default function MyTodoList() {
           value={description}
           onChange={(evt) => setDescription(evt.target.value)}
         />
-        <button>Adicionar</button>
+        <button disabled={mutation.isPending}>Adicionar</button>
       </form>
       <hr />
-      {tasks.length === 0 && <p>Adicione uma tarefa para exibir aqui.</p>}
+      {data?.results.length === 0 && (
+        <p>Adicione uma tarefa para exibir aqui.</p>
+      )}
       <ol>
-        {tasks.map((task) => (
-          <li key={task}>{task}</li>
+        {data?.results.map((task) => (
+          <li key={task.objectId}>{task.description}</li>
         ))}
       </ol>
       <hr />
